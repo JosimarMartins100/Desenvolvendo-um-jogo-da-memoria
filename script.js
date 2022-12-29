05b2ac22f3cff65d4de45f018cbb1ca146f582bf
Josimar Martins dev <josimarmartins2014@hotmail.com>
2022-12-29 00:32:32 -0300

    finalizando

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const cards = document.querySelectorAll('.card');
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let lockBoard = false;
 
 
 function flipCard() {
+    if(lockBoard) return;
+    if(this === firstCard) return;
+
     this.classList.add('flip');
     if(!hasFlippedCard){
         hasFlippedCard = true;
@@ -17,7 +21,7 @@ function flipCard() {
 }
 
 function checkForMatch(){
-    if(firstCard.dataset.card === secondCard.card){
+    if(firstCard.dataset.card === secondCard.dataset.card){
         disableCards();
         return;
     }
@@ -27,14 +31,30 @@ function checkForMatch(){
 function disableCards(){
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+    resetBoard();
 }
 
 function unflipCards(){
+    lockBoard = true;
     setTimeout(() => {
-        firstCard.classList.remov('flip');
+        firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+
+        resetBoard();
     }, 1500);
 }
+
+function resetBoard(){
+   [hasFlippedCard, lockBoard] = [false, false];
+   [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle(){
+    cards.forEach((card) => {
+        let randomPosition  = Math.floor(Math.random() * 12);
+        card.getElementsByClassName.order = randomPosition;
+    })
+})();
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
