200b06d8e45ced6fe2a9e26f4a3f8d1f9c68fb18
Josimar Martins dev <josimarmartins2014@hotmail.com>
2022-12-28 23:56:57 -0300

    produzindo

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 
 function flipCard() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+    if(!hasFlippedCard){
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMatch();
+}
+
+function checkForMatch(){
+    if(firstCard.dataset.card === secondCard.card){
+        disableCards();
+        return;
+    }
+    unflipCards();
+}
+
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards(){
+    setTimeout(() => {
+        firstCard.classList.remov('flip');
+        secondCard.classList.remove('flip');
+    }, 1500);
 }
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
 })
+
